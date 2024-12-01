8bb907c5a76743101dc5e2c0100daa172305d6b0
Diego <diegodemello027@gmail.com>
2024-12-01 12:50:17 -0300

    Adicionar arquivos de projeto.

---
+++ RectangleCalc/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace RectangleCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+
+            Console.WriteLine("Entre a largura e altura de um retângulo: ");
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("AREA = " + rectangle.area(width, height).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + rectangle.perimeter(width, height).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + rectangle.diagonal(width, height).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ RectangleCalc/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿namespace RectangleCalc
+{
+    internal class Rectangle
+    {
+        public double area(double width, double height)
+        {
+            return width * height;
+        }
+
+        public double perimeter(double width, double height)
+        {
+            return 2 *(width + height);
+        }
+
+        public double diagonal(double width, double height)
+        {
+            return Math.Sqrt(Math.Pow(width, 2) + Math.Pow(height, 2));
+        }
+    }
+}
